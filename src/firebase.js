90d2786e0ddf0d82432f3632b2f6fbd377c2d9e0
aditya0802 <adityanegandhi02@gmail.com>
2020-10-31 19:44:01 +0530

    created custom navbar

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAVWi1ZKuvXCF9Nw4gIzMhhhlmeuRSKjLo",
+    authDomain: "best-buys-2daec.firebaseapp.com",
+    databaseURL: "https://best-buys-2daec.firebaseio.com",
+    projectId: "best-buys-2daec",
+    storageBucket: "best-buys-2daec.appspot.com",
+    messagingSenderId: "576762406897",
+    appId: "1:576762406897:web:380f393e3ac7aee7730650",
+    measurementId: "G-8SHFD7MBM8"
+  };
