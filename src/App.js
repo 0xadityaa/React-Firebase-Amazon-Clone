f5dbe9044c3533622a8d5b1031f1458e07dc196f
aditya0802 <adityanegandhi02@gmail.com>
2020-11-01 12:53:32 +0530

    Created Body of Landing Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/*Home */}
+      <Home/>
     </div>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Product.css'
+function Product({title, image, price, rating}) {
+    return (
+        <div className='product'>
+            <div className='product__info'>
+                <p>{title}</p>
+                <p className='product__price'>
+                    <small>₹</small>
+    <strong>{price}</strong>
+                </p>
+                <div className='product__ratings'>
+                {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+                </div>
+            </div>
+            <img src={image} alt=''/>
+            <button>Add to basket</button>
+        </div>
+    )
+}
+
+export default Product
